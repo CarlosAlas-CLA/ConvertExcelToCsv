1578e3fa4bb8ecbfbd57d3fa8988eceef2e37571
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-05-22 11:18:31 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ExcelReader
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {//Path from file location
+            string fileLcation = @"C:\Users\carlo\Documents\";
+            //Class instance
+            ConvertFile newConvFile = new ConvertFile();
+            //User Information message
+            Console.WriteLine("Please enter xlsx workbook to convert or spreadsheet to convert  from your Documents ");
+            //Variable and user input
+            string inputfile = Console.ReadLine();
+            //Class instance method calling 
+            newConvFile.ReturnFile(fileLcation + inputfile);
+
+
+
+        }
+    }
+}
