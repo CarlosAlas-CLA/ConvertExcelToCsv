5e4beb43f47636a05e66f07018d740a0ce910066
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-26 14:13:10 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,16 +7,13 @@ namespace ExcelReader
     {
 
         static void Main(string[] args)
-        {//Path from file location
-            string fileLcation = @"C:\Users\carlo\Documents\";
-            //Class instance
-            ConvertFile newConvFile = new ConvertFile();
-            //User Information message
-            Console.WriteLine("Please enter xlsx workbook to convert or spreadsheet to convert  from your Documents ");
-            //Variable and user input
-            string inputfile = Console.ReadLine();
-            //Class instance method calling 
-            newConvFile.ReturnFile(fileLcation + inputfile);
+        {   //Files
+            string exceFile = @"C:\Users\Desktop\test-2019-09-24-09-56-17-9834.xlsx";
+            string outputCsvFile = @"C:\Users\Desktop\New folder\Output\converted.csv";
+            string log = @"C:\Users\carlo\Desktop\New folder\log.txt";
+            //Method
+      
+            ExcelTOCsv.excelToCsv(exceFile,outputCsvFile,log);
 
 
 
